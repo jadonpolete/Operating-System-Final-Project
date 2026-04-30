71b6e05405e6be5e8e7d0e8fed89a75e7f1d5abf
Jadon Polete <jadonlpolete@gmail.com>
2026-04-30 14:51:43 -0400

    feat: add tracemask field to process struct

--- kernel/proc.h
+++ kernel/proc.h
@@ -92,6 +92,8 @@ struct proc {
   int xstate;                  // Exit status to be returned to parent's wait
   int pid;                     // Process ID
 
+  // Syscall trace mask addition
+  int tracemask;
   // wait_lock must be held when using this:
   struct proc *parent;         // Parent process
 
