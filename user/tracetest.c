d122b2307eae96f039242f2b3ad1659486b9156f
Jadon Polete <jadonlpolete@gmail.com>
2026-04-30 15:48:27 -0400

    test: simplify trace test output

--- user/tracetest.c
+++ user/tracetest.c
@@ -8,8 +8,8 @@ main(int argc, char *argv[])
 {
   printf("Starting trace test\n");
 
-  // Enable tracing for getpid and write.
-  trace((1 << SYS_getpid) | (1 << SYS_write));
+  // Enable tracing for getpid only to keep output clean.
+  trace(1 << SYS_getpid);
 
   int pid = getpid();
   printf("Current pid: %d\n", pid);
