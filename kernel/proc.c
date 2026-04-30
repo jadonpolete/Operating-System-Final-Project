d1d31fa3c8812bb6c29235488e47476a58bc8b02
Jadon Polete <jadonlpolete@gmail.com>
2026-04-30 15:10:01 -0400

    feat: inherit tracemask in fork

--- kernel/proc.c
+++ kernel/proc.c
@@ -278,7 +278,7 @@ kfork(void)
 
   // copy saved user registers.
   *(np->trapframe) = *(p->trapframe);
-
+  np->tracemask = p->tracemask;  // inherit syscall tracing mask
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
 
